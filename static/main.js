9020f1f57c9873164f6f2747ab7d4044da8e3e61
Prakhar Gupta <prakhar18.linkedin@gmail.com>
2024-11-28 01:08:14 +0530

    Create main.js

---
+++ static/main.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".text", {
+    strings: ["Python Developer", "AI/ML Enthusiast", "Hackathon Winner", "Web Developer", "Tech Enthusiast"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+
+
